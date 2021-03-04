6355d8a9cc928e8925aec7c02af4efe8ecd1db6e
robsonmanata <70637849+robsonmanata@users.noreply.github.com>
2021-03-04 16:27:15 +0100

    Add files via upload
    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// responsive navigation bar
+
+function myResponsivebar() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+//dropdown button
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myDropdown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
